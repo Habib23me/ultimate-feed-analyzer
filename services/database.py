755590130435cf642c42ffb745594cc8354d2f61
Fitsum Ayalew <fitsumayalew047@gmail.com>
2022-06-20 03:03:44 +0300

    added custom functions

--- services/database.py
+++ services/database.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
-from util import Singleton
+from util import *
 import rocksdb3
 from time import time
-from util.helpers import convert_byte_string_to_json, convert_json_to_byte_string
+# from util.helpers import *
 
 
 load_dotenv()
@@ -26,7 +26,7 @@ class DatabaseService(metaclass=Singleton):
         values = list(self.db.get_iter())
         return list(map(self.tuple_to_json, values))
 
-    def store_data(self, value, key=None):
+    def put(self, value, key=None):
         if(key == None):
             key = str(time())
         self.db.put(key.encode('utf-8'), convert_json_to_byte_string(value))
@@ -39,11 +39,37 @@ class ActivityClusterDatabaseService(DatabaseService):
         super(DatabaseService, self).__init__(
             os.environ['DB_ACTIVITY_CLUSTER_LOC'])
 
+    def getActivitiesByCluster(self, cluster: int) -> list:
+        # get all keys from db
+        values = list(self.db.get_iter())
+        return list(map(self.tuple_to_json, values))
+
 
 class ClusterFeatureDatabaseService(DatabaseService):
     def __init__(self, ):
         super(DatabaseService, self).__init__(
             os.environ['DB_CLUSTER_FEATURE_LOC'])
+
+    def getFeaturesByClusterId(self, clusterId: int) -> list:
+        clusterFeatures = []
+        it = self.db.iteritems()
+        it.seek_to_first()
+        # check if there are any items in the db
+        for key, value in it:
+            value = value.decode('utf-8')
+            clusterFeature = ClusterFeature.ClusterFeatureFromString(value)
+            if (clusterFeature.clusterId == clusterId):
+                clusterFeatures.append(clusterFeature)
+        return clusterFeatures
+
+    def getClusterIdForFeature(self, cfs, feature: str) -> int:
+        clusterId = -1
+        type(cfs)
+        for cf in cfs:
+            if (cf.feature == feature):
+                clusterId = cf.clusterId
+                break
+        return clusterId
 
 
 class ActivityUserDatabaseService(DatabaseService):

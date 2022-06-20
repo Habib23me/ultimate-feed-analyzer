15de3051bebbcbe2987790c5493085433ac6a12b
Habib <habib23me@gmail.com>
2022-06-20 11:58:46 +0300

    Fixed model

--- services/cluster_features.py
+++ services/cluster_features.py
@@ -2,8 +2,8 @@ import joblib;
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def predictCluster(feature:str):
-    vectorizer=joblib.load('../vectorizer.pkl')
-    model=joblib.load('../model.pkl')
+    vectorizer=joblib.load('vectorizer.pkl')
+    model=joblib.load('model.pkl')
     # print(model.cluster_centers_)
     Y = vectorizer.transform([feature])
     prediction = model.predict(Y)[0];
